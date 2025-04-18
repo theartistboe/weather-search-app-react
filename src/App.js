73203e5bcf5cf2eb6ca7bf06f2a0acc6f629c368
theartistboe <ieliea3508@gmail.com>
2025-04-18 10:38:59 -0700

    added Github repo and url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">React Weather App</div>
+    <div className="App">
+      <h1>React Weather App</h1>
+      <footer>
+        This project was created by Isaboe Hollis and 
+        <a href='https://github.com/theartistboe/weather-search-app-react' target='_blank'>
+         is open-sourced on GitHub.</a>
+        </footer>
+      </div>
   );
 }
 
