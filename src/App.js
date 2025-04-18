e6eed6256a0edd34a2a24eb46fdcb49d18a2a12d
theartistboe <ieliea3508@gmail.com>
2025-04-18 11:13:43 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <h1>React Weather App</h1>
       <footer>
-        This project was created by Isaboe Hollis and 
-        <a href='https://github.com/theartistboe/weather-search-app-react' target='_blank'>
-         is open-sourced on GitHub.</a>
+        This project was created by Isaboe Hollis and  {" "}
+        <a href='https://github.com/theartistboe/weather-search-app-react' target='_blank' rel="noreferrer"> 
+        is open-sourced on GitHub.</a>
         </footer>
+        </div>
       </div>
   );
 }
