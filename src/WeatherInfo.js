2d1d4e9266bd67e56f06137fb8774a7fc8395348
theartistboe <ieliea3508@gmail.com>
2025-04-20 19:46:19 -0700

    added temperature unit conversion from F to C

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
@@ -18,8 +19,7 @@ export default function WeatherInfo(props) {
               <WeatherIcon code={props.data.icon} />
               </div>
             <div className="float-left">
-            <span className="temperature">{Math.round(props.data.temperature)}</span>
-            <span className="unit">°F</span>
+              <WeatherTemperature fahrenheit={props.data.temperature} />
             </div>
             </div>
           </div>
